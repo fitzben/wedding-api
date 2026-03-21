17482d8130a7fc07967c37dd1e6be437cbc6e377
Benjaas <itmanager@MacBook-Pro.local>
2026-03-22 00:46:00 +0700

    test auto deploy

--- src/routes/Admin/handlers/guestHandler.ts
+++ src/routes/Admin/handlers/guestHandler.ts
@@ -11,7 +11,7 @@ export async function handleGuests(
   env: Env,
   user: JWTPayload,
   method: string,
-  pathname: string
+  pathname: string,
 ) {
   if (pathname === "/api/admin/guests") {
     if (method === "GET") {
